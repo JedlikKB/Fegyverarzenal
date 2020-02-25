7342c640cf7450395e1612be7183acc584c504ce
Kozma.Barnabás <Kozma.Barnabs@C41-04>
2020-02-25 13:17:33 +0100

    Fegyverek készen vannak.

---
+++ fegyverarzenal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fegyverarzenal
+{
+    class Program
+    {
+        enum fegyverek { apaökle,uzi,lángszóró,kézigránát}
+        static void Main(string[] args)
+        {
+            fegyverek kezemben = fegyverek.kézigránát;
+            List<fegyverek> fegyvertár = new List<fegyverek>();
+            fegyvertár.Add(fegyverek.uzi);
+            fegyvertár.Add(fegyverek.lángszóró);
+            foreach (fegyverek fegyver in fegyvertár)
+            {
+                Console.WriteLine(fegyver);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
